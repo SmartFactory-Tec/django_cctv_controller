600d10d24bfc52c8bff1ea914a3296b24a221364
teapot2 <A01197460@itesm.mx>
2023-10-19 16:07:09 -0600

    Code restructuring and utility functions

--- api/consumers.py
+++ api/consumers.py
@@ -1,19 +1,21 @@
 import asyncio
 import cv2
 import threading
-import base64
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from .scripts.utility import getRTSPUrls, generateImagePacket
 
 
 class VideoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
+        camera_urls = getRTSPUrls("api/data/cameras.json")
+
+        print(camera_urls)
+
         urls = [
             "http://pendelcam.kip.uni-heidelberg.de/mjpg/video.mjpg",
-            "http://webcam.zvnoordwijk.nl:82/mjpg/video.mjpg",
-            "http://camera.golfatlantide.com:8080/mjpg/video.mjpg?resolution=640x360&compression=12&fps=1&dummy=garb",
-            "http://myrafjell.sodvin.no/mjpg/video.mjpg"
         ]
 
         self.cameras = [VideoCamera(url) for url in urls]
@@ -21,11 +23,17 @@ class VideoConsumer(AsyncWebsocketConsumer):
 
         try:
             while True:
+                # Asynchronous sleep to control the pace of the loop
                 await asyncio.sleep(0.1)
+
+                # Iterate through the camera IDs and corresponding camera objects
                 for camera_id, camera in zip(camera_ids, self.cameras):
-                    frame = camera.get_frame()
-                    jpg_as_text = base64.b64encode(frame)
-                    message = f"{camera_id}:{jpg_as_text.decode()}"
+                    # Generate data packet containing image-related information
+                    message = generateImagePacket(
+                        frame=camera.get_frame(),
+                        camera_id=camera_id,
+                        fps=camera.get_fps(),
+                    )
 
                     await self.send(message)
         except asyncio.CancelledError:
@@ -52,7 +60,6 @@ class VideoCamera(object):
 
         if ret:
             _, jpg = cv2.imencode(".jpg", frame)
-
             return jpg
 
     def update(self):
---
+++ api/scripts/utility.py
@@ -0,0 +1,63 @@
+import json
+import base64
+
+
+def getRTSPUrls(route):
+    """
+    Generates RTSP URLs based on IP Camera documentation and credentials
+
+    :param route: Route to .json file with camera data
+    :returns: RTSP URLs
+    """
+    camera_file = open(route)
+    data = json.load(camera_file)["data"]
+
+    urls = []
+
+    for camera in data:
+        mac = camera["mac"]
+        ip = camera["ip"]
+        pwd = camera["pwd"]
+
+        urls.append(f"rtsp://admin:{pwd}@{ip}:554/cam/realmonitor?channel=1&subtype=1")
+
+    camera_file.close()
+
+    return urls
+
+
+def generateImagePacket(*, frame, camera_id, resolution=None, timestamp=None, fps=None):
+    """
+    Generates an image packet as a JSON string.
+
+    :param frame: The image data to be encoded and included in the packet.
+    :type frame: bytes
+
+    :param camera_id: Identifier for the camera.
+    :type camera_id: str
+
+    :param resolution: (Optional) The resolution of the image.
+    :type resolution: str or None
+
+    :param timestamp: (Optional) The timestamp of when the image was captured.
+    :type timestamp: str or None
+
+    :param fps: (Optional) Frames per second of the video capture.
+    : type fps: int or None
+
+    :return: A JSON string containing the image packet information.
+    :rtype: str
+    """
+    packet = {
+        "camera_id": camera_id,
+        "frame": base64.b64encode(frame).decode(),
+    }
+
+    if timestamp is not None:
+        packet["timestamp"] = timestamp
+    if resolution is not None:
+        packet["resolution"] = resolution
+    if fps is not None:
+        packet["fps"] = fps
+
+    return json.dumps(packet)
