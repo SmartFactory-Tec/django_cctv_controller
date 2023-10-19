a1dea7d99962bbf35988c6c2bd07ad31bb8fbe1d
teapot2 <A01197460@itesm.mx>
2023-10-19 16:33:57 -0600

    Added public RTSP URLs

--- api/consumers.py
+++ api/consumers.py
@@ -15,6 +15,9 @@ class VideoConsumer(AsyncWebsocketConsumer):
 
         urls = [
             "http://pendelcam.kip.uni-heidelberg.de/mjpg/video.mjpg",
+            # "http://webcam.zvnoordwijk.nl:82/mjpg/video.mjpg",
+            # "http://camera.golfatlantide.com:8080/mjpg/video.mjpg?resolution=640x360&compression=12&fps=1&dummy=garb",
+            # "http://myrafjell.sodvin.no/mjpg/video.mjpg"
         ]
 
         self.cameras = [Camera(url) for url in urls]
